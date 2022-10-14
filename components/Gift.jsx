81c1f526c0b505b8c4d12d22934b4628a6f74b79
Heinrich Dapper <heinrichdapper98@gmail.com>
2022-10-13 22:29:22 -0200

    Criação do projeto e propriedades css

---
+++ components/Gift.jsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Gift.module.css"
+export default function Gift() {
+    return (
+        <div className={styles.gift}>
+            <div className={styles.top}></div>
+            <div className={styles.body}></div>
+            <div className={styles.giftBowVertical}></div>
+        </div>
+    )
+}
